3f2e9e0be8c38cc87d262b6b07194c3d1e1b7eff
Sam <143469813+fohy24@users.noreply.github.com>
2024-04-12 14:20:50 -0700

    Adding data efficiency dropdown (#96)
    * incorporate real data
    
    * fixed DuplicateIdError: Duplicate component id found in the initial layout: `panel_efficiency`
    
    ---------
    
    Co-authored-by: Rafe Chang <cchang5@student.ubc.ca>

--- src/components.py
+++ src/components.py
@@ -7,6 +7,7 @@ import altair as alt
 
 
 from .data import alt_data, price_df, gdf_ca, panel_df
+from .data import alt_data, price_df, gdf_ca, panel_df
 
 
 background = alt.Chart(gdf_ca).mark_geoshape(
@@ -58,9 +59,10 @@ num_pan_slider = dcc.Slider(id='num_pan_slider',
 
 ## Panel Efficiency
 pan_eff_dropdown = dcc.Dropdown(id='panel_efficiency', options=[{'label': name , 'value': name } for name  in panel_df["name "].unique()], value=None)
+pan_eff_dropdown = dcc.Dropdown(id='panel_efficiency', options=[{'label': name , 'value': name } for name  in panel_df["name "].unique()], value=None)
 
 ## Panel Comparison
-pan_com_dropdown = dcc.Dropdown(id='panel_efficiency', options=[{'label': name , 'value': name } for name  in panel_df["name "].unique()], value=None, multi=True)
+pan_com_dropdown = dcc.Dropdown(id='panel_comparison', options=[{'label': name , 'value': name } for name  in panel_df["name "].unique()], value=None, multi=True)
 
 ## Difference in savings card
 diff_sav_card = dbc.Card(id='diff_card', children=[dbc.CardHeader('Difference in Savings'), dbc.CardBody('$XXX /yr')]),
