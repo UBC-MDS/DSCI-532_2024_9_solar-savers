781649b79ca400f10f17441d900e01aca78b8615
Marco Bravo <marco.bravoml@udlap.mx>
2024-04-16 16:15:26 -0700

    Refined data reading after preprocessing data wrangling + added needed import

--- src/data.py
+++ src/data.py
@@ -1,27 +1,14 @@
 import os
 import geopandas as gpd
 import pandas as pd
-
+import pyarrow as pa
+from shapely import wkb
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 price_df = pd.read_csv("../data/raw/pricePerProvince.csv", encoding='latin1').sort_values(by='price per ¢/kWh', ascending=False)
-# sunlight_df = pd.read_json("../data/processed/kWh_poly.json", lines=True, encoding='latin1')
-
-json_file_path = '../data/processed/kWh_poly.json'
-df1 = gpd.read_file(json_file_path)
-alt_data = df1.to_crs(epsg=4326)  
-alt_data = alt_data.dropna(subset=['latitude', 'longitude'])
-
-file_path = '../data/raw/ne_50m_admin_1_states_provinces/ne_50m_admin_1_states_provinces.shp'
-gdf1 = gpd.read_file(file_path)
-gdf_ca = gdf1[gdf1['iso_a2'] == 'CA']
-mapping = {
-    'Yukon': 'Yukon Territory',
-    'Québec': 'Quebec'
-}
-gdf_ca['name'] = gdf_ca['name'].replace(mapping)
-
-
-panel_file_path = '../data/raw/panels.csv'
-panel_df = pd.read_csv(panel_file_path)
+alt_data = pd.read_parquet('../data/processed/kWh_poly.parquet')
+alt_data['geometry'] = alt_data['geometry'].apply(wkb.loads)
+alt_data = gpd.GeoDataFrame(alt_data)
+gdf_ca = gpd.read_parquet('../data/processed/ne_50m_admin_1_states_provinces.parquet')
+panel_df = pd.read_csv('../data/raw/panels.csv')
