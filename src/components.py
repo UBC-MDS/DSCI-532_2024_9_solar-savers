b4da4f416b538ded1b154ba6483ee001bfffd7f9
Marco Bravo <marco.bravoml@udlap.mx>
2024-04-17 10:37:22 -0700

    Fixed Merge conflict

--- src/components.py
+++ src/components.py
@@ -1,7 +1,6 @@
-from dash import Dash, html, dcc, callback, Output, Input, dash_table
+from dash import html, dcc, dash_table
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
-import pandas as pd
 import altair as alt
 
 from .data import alt_data, price_df, gdf_ca, panel_df
@@ -38,6 +37,16 @@ info_section = dbc.Collapse(
 )
 
 
+# Title
+title = html.H1(
+    ' Solar Savers',
+    style={
+        'backgroundColor': 'steelblue',
+        'color': 'white',
+        'font-size': '48px',
+}
+)
+
 ## Province & Region Selection Dropdowns
 province_dropdown = dcc.Dropdown(id='province_dropdown', 
                                  options=[{'label': province, 'value': province} for province in alt_data["Province"].unique()], 
@@ -48,17 +57,6 @@ region_dropdown = dcc.Dropdown(id='region_dropdown', options=[],
                                placeholder='Select Region...')
 
 ## Number of Panels
-# num_pan_slider = dcc.Slider(id='num_pan_slider',
-#                             min=0,
-#                             max=20,
-#                             step=1,
-#                             value=5,
-#                             marks={0: '0', 20: '20'},
-#                             tooltip={'always_visible': True,
-#                                       'placement': 'top'}, 
-#                             style = {'background-color': '#f8f9fa'}                
-#                             )
-
 num_pan_slider = html.Div(
     dcc.Slider(
         id='num_pan_slider',
@@ -91,8 +89,6 @@ background = alt.Chart(gdf_ca).mark_geoshape(
 ).project(
     type = 'transverseMercator',
     rotate=[90, 0, 0]
-    # scale = scale, 
-    # translate=translate
 ).properties(
     width=500,
     height=400
@@ -109,8 +105,6 @@ points = alt.Chart(alt_data).mark_circle().encode(
     ).project(
         type = 'transverseMercator',
         rotate=[90, 0, 0]
-        # scale = scale, 
-        # translate=translate
     )
 
 combined_chart = (background + points).configure_view(
@@ -122,8 +116,6 @@ altair_chart = (dvc.Vega(id="altair-chart",
                         spec=combined_chart.to_dict()))
 
 ## Energy Savings Card
-#ener_sav_card = dbc.Card(id='ener_card', children=[dbc.CardHeader('Energy Savings'), dbc.CardBody('XXX kWh/year')]),
-
 ener_sav_card = dbc.Card(
     id='ener_card',
     children=[
@@ -139,8 +131,6 @@ ener_sav_card = dbc.Card(
 )
 
 ## Savings
-#savings_card = dbc.Card(id='sav_card', children=[dbc.CardHeader('Financial Savings'), dbc.CardBody('$XXX /year')]),
-
 savings_card = dbc.Card(
     id='sav_card',
     children=[
@@ -170,7 +160,6 @@ cost_card = dbc.Card(
 )
 
 ## Payback period Card
-#payback_card = dbc.Card(id='payback_card', children=[dbc.CardHeader('Payback Period'), dbc.CardBody('X year')])
 payback_card = dbc.Card(
     id='payback_card', 
     children=[
@@ -197,7 +186,6 @@ price_info_card = dash_table.DataTable(price_df.to_dict('records'), [{"name": i,
 # optimization fuction 
 roof_width_input = dbc.Row(
     [
-        #dbc.Col(html.Div("Width:"), width=4),
         dbc.Col(dbc.Input(id='input-roof-width', type='number', placeholder='Enter width (meters)') ),
     ],
     className='mb-2'
@@ -205,7 +193,6 @@ roof_width_input = dbc.Row(
 
 roof_length_input = dbc.Row(
     [
-        #dbc.Col(html.Div("Length:"), width=4),
         dbc.Col(dbc.Input(id='input-roof-length', type='number', placeholder='Enter length (meters)')),
     ],
     className='mb-2'
@@ -215,4 +202,73 @@ roof_length_input = dbc.Row(
 output_panel_count = dbc.Row(
     html.Div(id='output-panel-count'),
     className='mt-2', 
+)
+
+
+# SIDE BAR WIDGETS
+# Roof Dimensions (for sidebar)
+roof_dimensions = dbc.Row(
+    [
+        dbc.Col(roof_length_input), dbc.Col(roof_width_input), 
+        output_panel_count
+    ], 
+    style={
+        'background-color': 'white',
+        'padding': 7,
+        'border-radius': 3
+    })
+# Savings Summary
+savings_summary = dbc.Row(
+    [ 
+        dbc.Col(ener_sav_card, width=6, style={'margin-right': 0, 'padding-right': 0}),  
+        dbc.Col(savings_card, width=6, style={'margin-left': 0, 'padding-left': 0})
+                                
+    ], 
+    style={'margin-left': 0, 'margin-right': 0}
+)
+
+# Cost & Payback Summary
+cost_summary = dbc.Row(
+    [
+        dbc.Col(cost_card, width=6, style={'margin-right': 0, 'padding-right': 0}),  
+        dbc.Col(payback_card, width=6, style={'margin-left': 0, 'padding-left': 0})
+    ], 
+    style={'margin-left': 0, 'margin-right': 0})
+
+# Sidebar layout
+sidebar = dbc.Col(
+    [
+        html.Div("Location"),
+        province_dropdown,
+        region_dropdown,
+        html.Br(),
+        html.Div("Roof Dimensions"),
+        roof_dimensions, 
+        html.Br(),
+        html.Div("Number of Panels"),
+        num_pan_slider,
+        html.Br(),
+        html.Div("Panel Type"),
+        pan_eff_dropdown, 
+        html.Br(),
+        savings_summary, 
+        cost_summary 
+    ],
+    style={
+        'background-color': '#e6e6e6',
+        'padding': 10,
+        'border-radius': 0
+    }
+)
+# BOTTOM BAR: PANEL COMPARISON
+bottombar = dbc.Row(
+    [
+        dbc.Col(['Panel Comparison', pan_com_dropdown]),
+        dbc.Col(comparison_graph), 
+        dbc.Col(diff_sav_card)     
+    ], 
+    style={
+        'background-color': 'rgba(173, 216, 230, 0.7)',
+        'padding': 10
+    }
 )
